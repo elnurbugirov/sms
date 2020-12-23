930521a03cb3bb5fada1069b8e1aafc79cf3aecd
elnurbugirov <info.elnur@yahoo.com>
2020-12-23 15:50:06 +0400

    database update

--- database/migrations/2020_12_23_055641_create_exam_results_table.php
+++ database/migrations/2020_12_23_055641_create_exam_results_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRolesTable extends Migration
+class CreateExamResultsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create('roles', function (Blueprint $table) {
+        Schema::create('exam_results', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,7 +26,6 @@ class CreateRolesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('roles');
+        Schema::dropIfExists('exam_results');
     }
 }
-
